d1218ddb428f4bd6ae72241f563f42bc88df3f98
Madao1a <leprcoin@gmail.com>
2024-03-22 09:13:52 +0400

    IF_HW_6_1

--- src/test/java/TestSel.java
+++ src/test/java/TestSel.java
@@ -6,28 +6,27 @@ import java.util.List;
 import pages.*;
 import webhooks.WebHooks;
 
-public class Test extends WebHooks {
+public class TestSel extends WebHooks {
     private final Page page = new Page();
     private final PageTest pageTest = new PageTest();
     private final JiraTask jiraTask = new JiraTask();
     private final CreateJiraTask createJiraTask = new CreateJiraTask();
 
-    private final String log = "AT2";
-    private final String pass = "Qwerty123";
     private int initTaskCount;
     private int afterTaskCount;
 
     @DisplayName("Проверка авторизации")
     @org.junit.jupiter.api.Test
     public void loginTest() {
-        page.login(log, pass);
+        page.login(System.getProperty("login"),System.getProperty("password"));
         Assertions.assertTrue(AuthorizationTask.isProfileItemDisplayed());
     }
 
-    @org.junit.jupiter.api.Test
+
     @DisplayName("Проверка проекта тест")
+    @org.junit.jupiter.api.Test
     public void projectTest() {
-        page.login(log, pass);
+        page.login(System.getProperty("login"),System.getProperty("password"));
         pageTest.projectTest();
         Assertions.assertTrue(AuthorizationTask.isTitleExists("Открытые задачи"));
     }
@@ -35,7 +34,7 @@ public class Test extends WebHooks {
     @org.junit.jupiter.api.Test
     @DisplayName("проверка полей задачи TestSelenium")
     public void taskTestSelenium(){
-        page.login(log, pass);
+        page.login(System.getProperty("login"),System.getProperty("password"));
         AuthorizationTask.waitSignIn();
         pageTest.searchText("TestSelenium");
         List<String> projectStatus = jiraTask.statusCheck();
@@ -46,7 +45,7 @@ public class Test extends WebHooks {
     @org.junit.jupiter.api.Test
     @DisplayName("Создание нового бага")
     public  void CreatJiraTask(){
-        page.login(log, pass);
+        page.login(System.getProperty("login"),System.getProperty("password"));
         pageTest.projectTest();
         initTaskCount = pageTest.countTask();
         createJiraTask.createNewTask("Дефект","Описание дефекта" );
@@ -58,7 +57,7 @@ public class Test extends WebHooks {
     @org.junit.jupiter.api.Test
     @DisplayName("Перевод статусов")
     public void CaStatus(){
-        page.login(log, pass);
+        page.login(System.getProperty("login"),System.getProperty("password"));
         pageTest.projectTest();
         createJiraTask.statusChange();
     }
